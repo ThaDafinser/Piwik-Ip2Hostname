183154ca2376347c414368d3b7bc2029d90543bc
Keckeis <martin.keckeis1@gmail.com>
2017-03-07 13:39:07 +0100

    piwik3

--- Ip2Hostname.php
+++ Ip2Hostname.php
@@ -2,9 +2,27 @@
 namespace Piwik\Plugins\Ip2Hostname;
 
 use Piwik\Plugin;
-use Piwik\Common;
-use Piwik\Db;
 
 class Ip2Hostname extends Plugin
 {
+
+    /**
+     *
+     * @see Piwik\Plugin::registerEvents
+     */
+    public function registerEvents()
+    {
+        return [
+            'Live.getAllVisitorDetails' => 'getAllVisitorDetails',
+        ];
+    }
+
+    public function getAllVisitorDetails(&$visitor, $details)
+    {
+        if (isset($visitor['visitIp']) && isset($details['location_hostname']) && $details['location_hostname'] != '') {
+            $visitor['visitIp'] = $visitor['visitIp'] . ' | ' . $details['location_hostname'];
+        } elseif (isset($details['location_hostname']) && $details['location_hostname'] != '') {
+            $visitor['visitIp'] = $details['location_hostname'];
+        }
+    }
 }
